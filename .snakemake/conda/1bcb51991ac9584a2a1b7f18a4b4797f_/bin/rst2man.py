60752f38d8bfd14b74c31b43ee622406fbc37a40
CuteVoldi <kursatkolay97@gmail.com>
2024-01-08 22:43:33 +0300

    Blast output

---
+++ .snakemake/conda/1bcb51991ac9584a2a1b7f18a4b4797f_/bin/rst2man.py
@@ -0,0 +1,27 @@
+#!/home/kursat21/PycharmProjects/Miuul/.snakemake/conda/1bcb51991ac9584a2a1b7f18a4b4797f_/bin/python
+
+# Author:
+# Contact: grubert@users.sf.net
+# Copyright: This module has been placed in the public domain.
+
+"""
+man.py
+======
+
+This module provides a simple command line interface that uses the
+man page writer to output from ReStructuredText source.
+"""
+
+import locale
+try:
+    locale.setlocale(locale.LC_ALL, '')
+except Exception:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+from docutils.writers import manpage
+
+description = ("Generates plain unix manual documents.  "
+               + default_description)
+
+publish_cmdline(writer=manpage.Writer(), description=description)
---
+++ scripts/tRNAscan.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+tRnascan-se -o $1 $2
